0cb32711fac7862877ab6412e9d5ee1cf7fa83db
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-05 17:26:40 +0200

    Kreirana klasa Operacije i definisane vrednosti za operacije

---
+++ src/main/java/rs/ac/bg/fon/nprog/transfer/Operacije.java
@@ -0,0 +1,29 @@
+package rs.ac.bg.fon.nprog.transfer;
+
+public class Operacije {
+	
+	public static final int LOGIN = 1;
+    public static final int KREIRAJ_KOREOGRAFA = 2;
+    public static final int SACUVAJ_KOREOGRAFA = 3;
+    public static final int UCITAJ_KOREOGRAFE = 4;
+    public static final int UCITAJ_KOREOGRAFA = 5;
+    public static final int PROMENI_KOREOGRAFA = 6;
+    public static final int PRONADJI_KOREOGRAFE = 7;
+    public static final int KREIRAJ_LOKACIJU = 8;
+    public static final int UCITAJ_LOKACIJE = 9;
+    public static final int KREIRAJ_NASTUP = 10;
+    public static final int UCITAJ_BALETSKEGRUPE = 11;
+    public static final int KREIRAJ_BALETSKOGIGRACA = 12;
+    public static final int UCITAJ_BALETSKEIGRACE = 13;
+    public static final int KREIRAJ_BALETSKUGRUPU = 14;
+    public static final int PROMENI_PODATKEBALETSKOGIGRACA = 15;
+    public static final int UCITAJ_BALETSKOGIGRACA = 16;
+    public static final int ZAPAMTI_UPLATEBALETSKOGIGRACA = 17;
+    public static final int UCITAJ_BALETSKUGRUPU = 18;
+    public static final int PROMENI_PODATKEBALETSKEGRUPE = 19;
+    public static final int PRONADJI_BALETSKEIGRACE = 20;
+    public static final int UCITAJ_NASTUPE = 21;
+    public static final int ZAPAMTI_NASTUPEBALETSKEGRUPE = 22;
+    public static final int OBRISI_KOREOGRAFA = 23;
+    
+}
