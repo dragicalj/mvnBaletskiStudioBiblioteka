f5a6ffd4480603668c1c3f2c4d1c9098902636cd
dragicalj <gagaljubisavljevic111@gmail.com>
2022-07-05 16:17:16 +0200

    Kreiran paket za domenske klase i domenska klasa Administrator, dodate get, set, toString i equals metode

---
+++ src/main/java/rs/ac/bg/fon/nprog/domen/Administrator.java
@@ -0,0 +1,89 @@
+package rs.ac.bg.fon.nprog.domen;
+
+import java.util.Objects;
+
+public class Administrator {
+	
+	private Long administratorID;
+    private String ime;
+    private String prezime;
+    private String korisnickoIme;
+    private String lozinka;
+    
+    public Administrator() {
+    }
+
+    public Administrator(Long administratorID, String ime, String prezime, String korisnickoIme, String lozinka) {
+        this.administratorID = administratorID;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+    }
+    
+    public Long getAdministratorID() {
+        return administratorID;
+    }
+
+    public void setAdministratorID(Long administratorID) {
+        this.administratorID = administratorID;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Administrator{" + "ime=" + ime + ", prezime=" + prezime + '}';
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Administrator other = (Administrator) obj;
+        if (!Objects.equals(this.korisnickoIme, other.korisnickoIme)) {
+            return false;
+        }
+        return true;
+    }
+}
