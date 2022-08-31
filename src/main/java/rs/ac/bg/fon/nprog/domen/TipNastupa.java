3f57fe8c9e6471d614dc21e05a0f56946e827732
dragicalj <gagaljubisavljevic111@gmail.com>
2022-08-31 23:33:33 +0200

    Dokumentovan enum TipNastupa

--- src/main/java/rs/ac/bg/fon/nprog/domen/TipNastupa.java
+++ src/main/java/rs/ac/bg/fon/nprog/domen/TipNastupa.java
@@ -1,5 +1,15 @@
 package rs.ac.bg.fon.nprog.domen;
 
+/**
+ * Enum koji predstavlja tipove nastupa baletskih grupa.
+ * Baletske grupe mogu nastupati na koncertima, takmicenjima ili karnevalima.
+ * 
+ * Svaki nastup je jedanog tipa.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public enum TipNastupa {
     KONCERT,TAKMICENJE,KARNEVAL
 }
