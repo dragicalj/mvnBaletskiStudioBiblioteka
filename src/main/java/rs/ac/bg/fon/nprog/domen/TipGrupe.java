3d6a6ee63b9a0d23093c5e3cd41db60ee991d771
dragicalj <gagaljubisavljevic111@gmail.com>
2022-08-31 23:31:42 +0200

    Dokumentovan enum TipGrupe

--- src/main/java/rs/ac/bg/fon/nprog/domen/TipGrupe.java
+++ src/main/java/rs/ac/bg/fon/nprog/domen/TipGrupe.java
@@ -1,5 +1,14 @@
 package rs.ac.bg.fon.nprog.domen;
 
+/**
+ * Enum koji predstavlja tipove baletskih grupa u baletskom studiju.
+ * Postoje mini,juniorske i seniorske baletske grupe.
+ * Svaka baletska grupa ima jedan tip.
+ * 
+ * @author Dragica Ljubisavljevic
+ * @version 1.0
+ *
+ */
 public enum TipGrupe {
     Mini,Junior,Senior
 }
